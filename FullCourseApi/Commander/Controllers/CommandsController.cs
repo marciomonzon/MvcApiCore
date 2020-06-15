76748d83c811e20ff2264cac5a6eddd0f828919f
Marcio <marcio.pcmonzon@gmail.com>
2020-06-15 18:31:37 -0300

    testando com automapper e dtos

--- FullCourseApi/Commander/Controllers/CommandsController.cs
+++ FullCourseApi/Commander/Controllers/CommandsController.cs
@@ -1,4 +1,6 @@
-﻿using Commander.Data;
+﻿using AutoMapper;
+using Commander.Data;
+using Commander.Dtos;
 using Commander.Models;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -14,27 +16,35 @@ namespace Commander.Controllers
     public class CommandsController : ControllerBase
     {
         private readonly ICommanderRepo _repository;
+        private readonly IMapper _mapper;
 
-        public CommandsController(ICommanderRepo repository)
+        public CommandsController(ICommanderRepo repository, IMapper mapper)
         {
             _repository = repository;
+            _mapper = mapper;
         }
 
         // GET api/commands
         [HttpGet]
-        public ActionResult <IEnumerable<Command>> GetAllCommands()
+        public ActionResult<IEnumerable<CommandReadDto>> GetAllCommands()
         {
             var commandsItems = _repository.GetAllCommands();
 
-            return Ok(commandsItems);
+            return Ok(_mapper.Map<IEnumerable<CommandReadDto>>(commandsItems));
         }
 
         // GET api/commands/{id}
         [HttpGet("{id}")]
-        public ActionResult <Command> GetCommandById(int id)
+        public ActionResult<CommandReadDto> GetCommandById(int id)
         {
             var commandItem = _repository.GetCommandById(id);
-            return Ok(commandItem);
+
+            if (commandItem != null)
+            {
+                return Ok(_mapper.Map<CommandReadDto>(commandItem));
+            }
+
+            return NotFound();
         }
     }
 }
--- FullCourseApi/Commander/Dtos/CommandReadDto.cs
+++ FullCourseApi/Commander/Dtos/CommandReadDto.cs
@@ -1,24 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Commander.Models
+namespace Commander.Dtos
 {
-    public class Command
+    public class CommandReadDto
     {
-        [Key]
         public int Id { get; set; }
 
-        [Required]
-        [MaxLength(250)]
         public string HowTo { get; set; }
 
-        [Required]
         public string Line { get; set; }
 
-        [Required]
         public string Platform { get; set; }
     }
 }
